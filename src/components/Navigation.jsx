7a4536ca0edab7f595880c26db50762ae859b0f3
Ismail <119864188+ismailmail123@users.noreply.github.com>
2024-06-07 01:15:07 +0800

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -25,6 +25,7 @@ export const Navigation = () => {
 
   return (
     <> 
+      <div style={{height:"200px"}}>container</div>
       {['md'].map((expand) => (
         <Navbar key={expand} expand={expand} style={{position:"sticky", top:"0", backdropFilter:"blur(45)"}} className="mb-3 bg-success p-2 text-dark bg-opacity-50 z-index-3">
             <Container fluid>
